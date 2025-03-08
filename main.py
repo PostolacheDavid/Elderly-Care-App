f61309c47e20cab067d2b2cd624e529b384d3ca0
PostolacheDavid <davidp6plus@gmail.com>
2025-03-08 17:15:22 +0200

    Finished the python logic

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
-class MainScreen(MDScreen):
+USER_CREDITENTIALS = {
+    "doctor": "doctor123",
+    "elder": "elder123",
+    "caregiver": "caregiver"
+}
+
+class LoginScreen(Screen):
+    def login(self):
+        username = self.ids.username.text.strip()
+        password = self.ids.password.text.strip()
+
+        if username in USER_CREDITENTIALS and USER_CREDITENTIALS[username] == password:
+            self.manager.current = "main"
+        else:
+            self.ids.login_status.text = "[color=ff0000]Invalid Creditentials![/color]"
+
+class MainScreen(Screen):
     pass
 
 class MainApp(MDApp):
     def build(self):
-        return MainScreen()
+        return Builder.load_file("main.kv")
     
     def on_menu_click(self):
         print("Menu button clicked!")
